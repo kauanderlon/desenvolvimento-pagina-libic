42103246ca4ceae2c42bf19b9f004a566877ba50
Lauane <lauanemoraisarj@gmail.com>
2025-10-29 16:51:12 -0300

    Feat: Criação do footer

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     carregarHeader();
+    carregarFooter();
 });
 
 async function carregarHeader() {
@@ -21,5 +22,27 @@ async function carregarHeader() {
     } catch (error) {
         console.error("Falha ao buscar o header:", error);
         headerPlaceholder.innerHTML = "<p style='color:red;'>Erro ao carregar o menu.</p>";
+    }
+}
+
+async function carregarFooter() {
+    
+    const footerPlaceholder = document.getElementById("footer-placeholder");
+    
+    if (!footerPlaceholder) {
+        return; 
+    }
+
+    
+    try {
+        const response = await fetch("footer.html");
+        
+        const html = await response.text();
+        
+        footerPlaceholder.innerHTML = html;
+
+    } catch (error) {
+        console.error("Falha ao buscar o footer:", error);
+        footerPlaceholder.innerHTML = "<p style='color:red;'>Erro ao carregar o menu.</p>";
     }
 }
