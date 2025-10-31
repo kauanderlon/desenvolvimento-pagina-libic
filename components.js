488f052322842be43593556b20ef66b67aea3a12
Lauane <lauanemoraisarj@gmail.com>
2025-10-30 22:22:23 -0300

    Feat: Adição do footer

--- components.js
+++ components.js
@@ -1,3 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    carregarHeader();
+    carregarFooter();
+});
+
+async function carregarHeader() {
+    
+    const headerPlaceholder = document.getElementById("header-placeholder");
+    
+    if (!headerPlaceholder) {
+        return; 
+    }
+
+    
+    try {
+        const response = await fetch("header.html");
+        
+        const html = await response.text();
+        
+        headerPlaceholder.innerHTML = html;
+
+    } catch (error) {
+        console.error("Falha ao buscar o header:", error);
+        headerPlaceholder.innerHTML = "<p style='color:red;'>Erro ao carregar o menu.</p>";
+    }
+}
+
+async function carregarFooter() {
+    
+    const footerPlaceholder = document.getElementById("footer-placeholder");
+    
+    if (!footerPlaceholder) {
+        return; 
+    }
+
+    
+    try {
+        const response = await fetch("footer.html");
+        
+        const html = await response.text();
+        
+        footerPlaceholder.innerHTML = html;
+
+    } catch (error) {
+        console.error("Falha ao buscar o footer:", error);
+        footerPlaceholder.innerHTML = "<p style='color:red;'>Erro ao carregar o menu.</p>";
+    }
+}
+
 function criarSobreLigaComponent() {
     const container = document.createElement('div');
     container.className = 'container1';
@@ -45,4 +94,3 @@ document.addEventListener('DOMContentLoaded', () => {
         placeholder.appendChild(criarSobreLigaComponent());
     }
 });
-
